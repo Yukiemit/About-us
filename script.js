aa351951fdb73e6fcea7c0cd5c141cb2da903c7e
yukiemit <202310242@fit.edu.ph>
2025-03-18 13:01:29 +0800

    Fix description

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Website loaded successfully!");
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const cartCount = document.getElementById("cart-count");
+
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    cartCount.innerText = cart.length;
+
+});
